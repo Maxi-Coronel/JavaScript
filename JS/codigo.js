3f020aabde78961c0ea26d6ca34e9a4cc794e91c
Maxi Coronel <maxicoronel1992@gmail.com>
2021-04-22 20:19:54 -0300

    Clase-2

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,25 +1,54 @@
-let edad = parseInt(prompt(`ingrese su edad`));
-let saludo = prompt(`dime un saludo`);
-let dinero = prompt(`dime cuanto dinero quieres gastar`);
+//Clase 2
 
-if (edad < 18){
-    alert(`no puedes ingresar, usted es menor`);
-}else{
-    console.log(`Usted puede navegar en nuestra web`);
+let nombre = prompt(`Dime su nombre`);
+let edad = parseInt(prompt(`Ingrese su edad`));
+let saludo = prompt(`Dime un saludo`);
+saludo = saludo.toLowerCase();
+let dinero = parseInt(prompt(`Dime cuanto dinero quieres gastar`));
+
+
+if (saludo == `hola`){
+    console.log(saludo + ` ` + nombre + ` es un gusto conocerte.`);
+} else{
+    console.log(saludo + ` ` + nombre);
 };
 
-if (saludo == `Hola`){
-    alert(`Su saludo preferido es "Hola" muchas gracias por contestar`);
-} else{
-    console.log(`su saludo fue ` + saludo);
+if (edad < 18){
+    console.log(`no puedes ingresar, usted es menor`);
+}else{
+    console.log(nombre + `, usted puede navegar en nuestra web`);
 };
 
 if (dinero < 200){
-    alert(`con su dinero no puede comprar nada en nuestra tienda`);
+    console.log(`con su dinero no puede comprar nada en nuestra tienda`);
 }else if (dinero < 500){
-    alert(`con su dinero puede comprar solo una bebida`);
+    console.log(`con su dinero puede comprar solo una bebida`);
 }else if (dinero < 1000){
     console.log(`usted podrá comprar una bebida más un bocadito`);
 }else{
-    alert(`usted podrá comprar lo que quiera`);
+    console.log(`usted podrá comprar lo que quiera`);
 };
+
+
+//Clase 3
+
+/* for(let rep = 1; rep <= 10; rep++){
+    console.log(rep);
+};
+
+let numero = parseInt(prompt(`Ingrese un número`));
+
+for(let i = 1; i <= 10; i++){
+    console.log(numero + `x` + i + `=` + numero*i);
+};
+
+let repetir = false;
+
+do{
+console.log("¡Solo una vez!");
+}while(repetir);
+
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}; */
